f7649e1398ab4aed9930a5221f2594268d19722a
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-08-26 21:41:46 +0530

    Analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,12 @@
-import ClientLayout from "./clientLayout"
+import ClientLayout from "./clientLayout";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({ children }) {
-  return <ClientLayout>{children}</ClientLayout>
+  return <ClientLayout>{children}</ClientLayout>;
 }
 
-
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-      generator: 'v0.app'
-    };
+  generator: "v0.app",
+};
