93d38bacd1ae48f51a2baa8c636b5492217d26d7
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-08-26 21:46:16 +0530

    Analytics and Speed Insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,13 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }) {
-  return <ClientLayout>{children}</ClientLayout>;
+  return (
+    <ClientLayout>
+      {children}
+      <SpeedInsights />
+      <Analytics />
+    </ClientLayout>
+  );
 }
 
 import "./globals.css";
